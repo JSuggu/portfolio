a78e5d0f36a62494142b81d1c4f53687068d7a78
Franco Azame <azamefranco@gmail.com>
2024-08-10 02:28:58 -0300

    install tailwind package. also added gitignore file. project data and styles of the page were updated

--- index.js
+++
@@ -1,7 +0,0 @@
-import { services } from "./services/services.js";
-
-window.addEventListener("DOMContentLoaded", (e) => {
-    services.changeTheme();
-    services.contactRedirection();
-    services.sendEmail();
-});
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { services } from "./services/services.js";
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    services.contactRedirection();
+});
